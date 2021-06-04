7b7efe523bdae765d68d514adedb350152cdabdd
Aro <arayiksuqiasyan20@gmail.com>
2021-06-05 01:45:23 +0500

    responsive

--- detail.js
+++ detail.js
@@ -16,9 +16,32 @@ const frontimg = document.querySelector('.frontimg')
 const back = document.querySelector('.back')
 
 let img = id
-
+let count = 0
+let dis = false;
+let time;
 container.addEventListener('click', function () {
-    cards.classList.toggle('active')
+    // cards.classList.toggle('active')
+    clearTimeout(time)
+    if (!dis) {
+        dis = true;
+        count++
+        if (count === 2) {
+            cards.style.transform = `rotateY(${count * 180}deg)`
+            time = setTimeout(() => {
+                cards.style.transition = "unset"
+                cards.style.transform = `rotateY(0deg)`
+                setTimeout(() => {
+                    cards.style.transition = "0.5s all ease-in-out"
+                }, 100);
+                count = 0
+            }, 500);
+        } else {
+            cards.style.transform = `rotateY(${count * 180}deg)`
+        }
+        setTimeout(() => {
+            dis = false
+        }, 500);
+    }
 })
 
 
@@ -26,8 +49,11 @@ async function imgPokemon() {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
     const pokemon = await res.json()
     const div = document.createElement('div')
+    const h2 = document.createElement('h2')
+
+    h2.innerHTML = pokemon.name
     div.innerHTML = `
-    <h2>${pokemon.name}</h2>
+    
     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${img}.png" alt="#">
 <div class="height"><h2>Height${pokemon.height}</h2></div>
 <div class="height"><h2>Weight${pokemon.weight}</h2></div>
@@ -35,9 +61,14 @@ async function imgPokemon() {
 <div class="height"><h2>Base experience${pokemon.base_experience}</h2></div>
 <div class="height"><h2>${pokemon.types[0].type.name}</h2></div>
     `
-    div.style.display = 'flex'
-    div.style.justifyContent = 'center'
-    div.style.flexDirection = 'column'
+    // div.style.display = 'flex'
+    // div.style.justifyContent = 'center'
+    // div.style.flexDirection = 'column'
+    div.style.cssText = `
+        width:120px;
+        cursor:pointer
+    `
+    frontimg.append(h2)
     frontimg.append(div)
     console.log(pokemon);
     return pokemon
